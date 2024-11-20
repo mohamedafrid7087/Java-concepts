c8297525ded11e461cc2fcbefb0374e39c07c427
Mohamed Afrid <meetafrid@gmail.com>
2024-11-20 22:34:23 +0000

    sum of the number 1 to N so the user enter the number of choice, if he enters 5 then ans is 15

--- src/com/loops/sumOfNumbers.java
+++ src/com/loops/sumOfNumbers.java
@@ -8,7 +8,7 @@ public class sumOfNumbers {
         System.out.println("Ask user to enter any number");
         int count =sc.nextInt();
         int initialize =0;
-        for(int i=1; i<count; i++){
+        for(int i=1; i<count; i++){ // starting from 1
             initialize+=i;
         }
 
