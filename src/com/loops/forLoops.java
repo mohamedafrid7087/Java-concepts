41653e8d6de552eee72b8557265056ad84f91996
Mohamed Afrid <meetafrid@gmail.com>
2024-11-20 22:09:00 +0000

    First loop concept trial to add in git

---
+++ src/com/loops/forLoops.java
@@ -0,0 +1,10 @@
+package com.loops;
+
+public class forLoops {
+
+    public static void main(String[] args) {
+        for(int i=0; i<=5; i++){
+            System.out.println("count " + i);
+        }
+    }
+}
